9a50662f496386bc4907996c7267e889dd6d59c2
shivansh7310 <uvshivansh.aws3012@gmail.com>
2025-06-27 13:39:48 +0530

    4th

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var express = require(‘express’);
  
 var app = express();
-//Respond with "hello world" for requests that hit our root "/"
+//Respond with "hello mukta" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
+ res.send(‘hello mukta’);
 });
 //listen to port 80 by default
 app.listen(process.env.PORT || 80);
